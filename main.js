3a42e977c6795f76133d120f10d9a887937ddf92
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-04-27 17:16:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-console.log("ml5 Version is" , ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/htZrtVHmf/model.json", modelLoaded);
-function modelLoaded(){
-
-console.log("Model Is Loaded :)");
-} 
-
 Webcam.set({
 
     height : 300,
@@ -25,3 +18,113 @@ Webcam.set({
     } );
     
     }
+
+console.log("ml5 Version is" , ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/39m9hAbCq/model.json", modelLoaded);
+function modelLoaded(){
+
+console.log("Model Is Loaded :)");
+} 
+
+function Speak(){
+
+    var Synth = window.speechSynthesis;
+    
+    speak_data1 = "The first prediction is " + prediction_1;
+    speak_data2 = "and the second prediction is "+ prediction_2;
+    
+    var utterThis = new SpeechSynthesisUtterance(speak_data1+speak_data2)
+    Synth.speak(utterThis);
+    }
+    
+    function predict_gesture(){
+    
+    i = document.getElementById("cap_pic");   
+    
+    classifier.classify(i,gotResult);
+    
+    }
+    
+    function gotResult(error,results){
+    
+    if(error){
+    
+    console.error(error)
+    
+    }
+    else{
+    
+    console.log(results)
+    document.getElementById("prediction_1_name").innerHTML = results[0].label;
+    document.getElementById("prediction_2_name").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+
+    Speak()
+
+    function predict_gesture(){
+
+    if(results[0].label == "Raising Finger"){
+
+document.getElementById("prediction_1_gesture").innerHTML = "☝"; }
+
+else if(results[0].label == "Fist"){
+
+document.getElementById("prediction_1_gesture").innerHTML = "✊";}
+
+else if(results[0].label == "Raising hand"){
+
+    document.getElementById("prediction_1_gesture").innerHTML = "✋";}
+
+else if(results[0].label == "Victory"){
+
+ document.getElementById("prediction_1_gesture").innerHTML = "✌";}
+
+ else if(results[0].label == "Backhand pointing up index finger"){
+
+    document.getElementById("prediction_1_gesture").innerHTML = "👆";}
+    
+else if(results[0].label == "Backhand pointing down index finger"){
+
+        document.getElementById("prediction_1_gesture").innerHTML = "👇";}
+
+else if(results[0].label == "Backhand pointing left index finger"){
+
+ document.getElementById("prediction_1_gesture").innerHTML = "👈";}
+ 
+ //------------------------------------------------------------------
+
+ if(results[1].label == "Raising Finger"){
+
+    document.getElementById("prediction_2_gesture").innerHTML = "☝"; }
+    
+    else if(results[1].label == "Fist"){
+    
+    document.getElementById("prediction_2_gesture").innerHTML = "✊";}
+    
+    else if(results[1].label == "Raising hand"){
+    
+        document.getElementById("prediction_2_gesture").innerHTML = "✋";}
+    
+    else if(results[1].label == "Victory"){
+    
+     document.getElementById("prediction_2_gesture").innerHTML = "✌";}
+    
+     else if(results[1].label == "Backhand pointing up index finger"){
+    
+        document.getElementById("prediction_2_gesture").innerHTML = "👆";}
+        
+    else if(results[1].label == "Backhand pointing down index finger"){
+    
+            document.getElementById("prediction_2_gesture").innerHTML = "👇";}
+    
+    else if(results[1].label == "Backhand pointing left index finger"){
+    
+     document.getElementById("prediction_2_gesture").innerHTML = "👈";}
+        
+}
+
+}
+}
+
